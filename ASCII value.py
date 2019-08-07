65b6c33ea5949086c3fa2231a7a92bd5e7920cf6
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-07 14:28:12 +0530

    Update ASCII value.py

--- ASCII value.py
+++ ASCII value.py
@@ -1,6 +1,8 @@
+#Python program to get the ASCII value of a character
 print()
 print(ord('a'))
 print(ord('A'))
 print(ord('1'))
 print(ord('@'))
 print()
+Python program to get the ASCII value of a character
