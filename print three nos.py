30f13faa09db47f9f974a1ea09c86297257c3d7d
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-07 14:01:07 +0530

    Create print three nos.py

---
+++ print three nos.py
@@ -0,0 +1,19 @@
+def removenums(int_list):
+    position=3-1
+    idx=0
+    len_list=(len(int_list))
+    while len_list>0:
+        idx=(position+idx)%len_list
+        print(int_list.pop(idx))
+        len_list-=1
+nums=[10,20,30,40,50,60,70,80,90]
+removenums(nums)def removenums(int_list):
+    position=3-1
+    idx=0
+    len_list=(len(int_list))
+    while len_list>0:
+        idx=(position+idx)%len_list
+        print(int_list.pop(idx))
+        len_list-=1
+nums=[10,20,30,40,50,60,70,80,90]
+removenums(nums)
