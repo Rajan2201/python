9c64c8dc9a387f113f2ae0fc3e09a0d007f9d819
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-09 12:36:06 +0530

    Create two dict add values.py

---
+++ two dict add values.py
@@ -0,0 +1,17 @@
+# Python program to combine two dictionary adding values for common keys
+d1={'a':200,'b':100,'c':300}
+d2={'d':500,'c':700,'e':200}
+for key in d2:
+    if key in d1:
+        d2[key]=d2[key]+d1[key]
+    else:
+        print("no common keys")
+print(d2)
+d1={'a':200,'b':100,'c':300}
+d2={'d':500,'c':700,'e':200}
+for key in d2:
+    if key in d1:
+        d2[key]=d2[key]+d1[key]
+    else:
+        pass
+print(d2)
