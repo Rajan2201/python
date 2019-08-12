5df61c5d8d6fe7985102db2f3c205ec1dd8cdf97
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-12 15:55:30 +0530

    Update reverse  string.py

--- reverse  string.py
+++ reverse  string.py
@@ -1,4 +1,8 @@
 # Python program to reverse a string word by word
-str1="rajan"
-for i in str1.split():
-    print(i[::-1])
+s=input()
+l=[]
+for i in s.split():
+    l.append(i)
+l.reverse()
+print(*l)
+
