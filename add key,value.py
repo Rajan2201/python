afafe99ae37e79be7ad929a9bd32815c8230ee5d
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-12 14:24:14 +0530

    Create add key,value.py

---
+++ add key,value.py
@@ -0,0 +1,5 @@
+ #Python program to get the key, value and item in a dictionary
+ d1={"name":"rajan","age":19,"address":"vadalur"}
+print("keys:",d1.keys())
+print("value:",d1.values())
+print("items:",d1.items())
