5f01e774f93c319136ddfe835102a965f2d4c74b
Rajan2201 <53602864+Rajan2201@users.noreply.github.com>
2019-08-12 14:17:50 +0530

    Update two dict add values.py

--- two dict add values.py
+++ two dict add values.py
@@ -7,11 +7,3 @@ for key in d2:
     else:
         print("no common keys")
 print(d2)
-d1={'a':200,'b':100,'c':300}
-d2={'d':500,'c':700,'e':200}
-for key in d2:
-    if key in d1:
-        d2[key]=d2[key]+d1[key]
-    else:
-        pass
-print(d2)
